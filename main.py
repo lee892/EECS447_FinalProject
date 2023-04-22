945b50a07578adf18807c9ab4e881521b728e0bc
Ahmni <ahmni@ku.edu>
2023-04-21 20:10:42 -0500

    init

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ try:
         host="localhost",
         user="root",
         password="password",
-        port=3306
+        port=3306,
+        database="SpotifyDB_Test"
     ) as connection:
         print(connection)
+        with connection.cursor() as cursor:
+            cursor.execute(create_db_query)
 except Error as e:
     print(e)
+
